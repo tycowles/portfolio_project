0952153bae9efde05e1775968f7c78bc6bf0ac69
tycowles <tyleracowles@gmail.com>
2024-11-27 11:08:04 +0000

    setting up DB

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 // Setup express and ejs
 var express = require ('express')
 var ejs = require('ejs')
+var mysql = require('mysql2');
 
 // Create the express application object
 const app = express()
 const port = 8000
 
+// Define the database connection
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'portfolio_app',
+    password: 'qwertyuiop',
+    database: 'portfolio'
+});
+
+// Connect to the database
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+}); 
+global.db = db; 
+
 // Tell Express that we want to use EJS as the templating engine
 app.set('view engine', 'ejs');
+
+// Set up the body parser 
+app.use(express.urlencoded({ extended: true })); 
+
+// Set up css
+app.use(express.static(__dirname + '/public'));
 
 // Load the route handlers
 const mainRoutes = require("./routes/main");  
--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = express.Router();
 
+var shopData = {shopName: "Bertie's Books"}
 
 
 // Handle the main routes
@@ -9,7 +10,95 @@ router.get('/',(req,res) => {
     res.render('index.ejs')
  });
 
- router.get('*', (req, res) => {
+router.get('/about',function(req,res){
+    res.render('about.ejs', shopData);
+});
+
+// router.get('/search',function(req,res){
+//     res.render("search.ejs", shopData);
+// });
+
+ router.get('/register',(req,res) => {
+    res.render('register.ejs')
+ });
+
+router.get('/addProduct', function (req,res) {
+    res.render('addProduct.ejs', shopData);                                                                     
+}); 
+  
+router.get('/showProduct', function (req,res) {
+    res.render('showProduct.ejs', shopData);                                                                     
+}); 
+  
+router.post('/registered', function (req,res) {
+    // saving data in database
+    let sqlquery = "INSERT INTO users (email, password) VALUES (?,?)";
+    // execute sql query
+    let newrecord = [req.body.email, req.body.password];
+    db.query(sqlquery, newrecord, (err, result) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    else {
+        res.send(' This user is added to database, email: '
+                + req.body.email);
+    }
+    });
+}); 
+
+router.post('/productadded', function (req,res) {
+    // saving data in database
+    let sqlquery = "INSERT INTO products (user_id, name, description, price) VALUES (?,?,?,?)";
+    // execute sql query
+    let newrecord = [req.body.user_id, req.body.name, req.body.description, req.body.price];
+    db.query(sqlquery, newrecord, (err, result) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      else {
+        res.send(' This product is added to database, name: '
+                  + req.body.name + ' description '+ req.body.description + ' price '+ req.body.price);
+      }
+    });
+}); 
+  
+  router.get('/search-result', function (req,res) {
+      // saving data in database
+      let sqlquery = "SELECT * FROM books WHERE name = ?";
+      // execute sql query
+      let keyword = [req.query.keyword];
+      console.log(req.query.keyword);
+  
+      db.query(sqlquery, keyword, (err, result) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        else {
+          res.send(' We do have this book, name: '
+                    + result[0].name + ' price '+ result[0].price);
+        }
+      });
+  }); 
+  
+  router.get('/showResult', function (req,res) {
+    // saving data in database
+    let sqlquery = "SELECT * FROM products WHERE user_id = ?";
+    // execute sql query
+    let keyword = [req.query.user_id];
+    console.log(req.query.keyword);
+  
+    db.query(sqlquery, keyword, (err, result) => {
+      if (err) {
+        res.redirect('./'); 
+      }
+      //res.send(result)
+      let newData = Object.assign({}, shopData, {products:result});
+        console.log(newData)
+        res.render("showResult.ejs", newData)
+    });
+  });
+
+router.get('*', (req, res) => {
     res.status(404).send("<div>404 Not Found</div>");
 });
 
