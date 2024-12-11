f57fc489106ba95aee82ba7ba6cdb2415a1ca546
tycowles <tyleracowles@gmail.com>
2024-12-11 12:22:41 +0000

    db permission

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const port = 8000
 
 // Define the database connection
 const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'portfolio_app',
-    password: 'qwertyuiop',
-    database: 'portfolio'
-
     // host: 'localhost',
-    // user: 'uni_helper_app',
+    // user: 'portfolio_app',
     // password: 'qwertyuiop',
-    // database: 'uni_helper'
+    // database: 'portfolio'
+
+    host: 'localhost',
+    user: 'uni_helper_app',
+    password: 'qwertyuiop',
+    database: 'uni_helper'
 });
 
 // Connect to the database
